0d8698d01cee48be0ce8325a0ee8944b3fba4fa2
Nikunj Kumar Gupta <nikunj.gupta447@gmail.com>
2017-12-20 18:00:57 +0530

    file execution fixed

--- MyServer.java
+++ MyServer.java
@@ -1,32 +1,41 @@
 import java.net.*;
+import java.awt.Desktop;
 import java.io.*;
 
 class MyServer {
 	public static void main(String args[]) throws Exception {
 		ServerSocket ss = new ServerSocket(3333);
 		Socket s = ss.accept();
-		
-		BufferedReader in=new BufferedReader(new InputStreamReader(s.getInputStream()));
-		
-		PrintWriter out=new PrintWriter(new BufferedWriter(new OutputStreamWriter(s.getOutputStream())), false);
-		
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+		PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(s.getOutputStream())), false);
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		String str = "", str2 = "";
 		while (!str.equals("stop")) {
 			str = in.readLine();
-			
-			if(!(new File(str).isDirectory())){
-				Runtime.getRuntime().exec("vlc "+str);
-				out.println("EXECUTED");
-				out.flush();
+
+			if (!(new File(str).isDirectory())) {
+				Desktop desktop = Desktop.getDesktop();
+				try {
+					desktop.open(new File(str));
+				} catch (IOException e1) {
+					System.out.println("error");
+				}
 				continue;
 			}
-			
+
 			System.out.println("client says: " + str);
 			out.println("START_MSG");
-			for(File f : new File(str).listFiles()) {
-				out.println(f.getName());
+			for (File f : new File(str).listFiles()) {
+				if (f.getName().charAt(0) == '.')
+					continue;
+				if (f.isDirectory())
+					out.println((char) 16 + f.getName());
+				else
+					out.println((char) 17 + f.getName());
 			}
 			out.println("END_MSG");
 			out.flush();
